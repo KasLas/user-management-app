f47b4e3563ac463adb92ecc89e3bec7c8c73558a
Kaspars Lasinskis <kasparsll@gmail.comm>
2021-07-13 12:03:41 +0300

    Added Card and styled the form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,28 @@
 import React from "react";
+import styled from "styled-components";
+import Card from "../UI/Card";
+
+const StyledForm = styled.form`
+  & label {
+    display: block;
+    font-weight: bold;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    font: inherit;
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    padding: 0.15rem;
+    margin-bottom: 0.5rem;
+  }
+
+  & input:focus {
+    outline: none;
+    border-color: #4f005f;
+  }
+`;
 
 const AddUser = () => {
   const addUserHandler = (event) => {
@@ -7,15 +31,15 @@ const AddUser = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={addUserHandler}>
+    <Card>
+      <StyledForm onSubmit={addUserHandler}>
         <label htmlFor="userName">Username</label>
         <input id="userName" type="text" />
         <label htmlFor="userAge">Age (years)</label>
         <input id="userAge" type="number" />
         <button type="submit">Add User</button>
-      </form>
-    </div>
+      </StyledForm>
+    </Card>
   );
 };
 
